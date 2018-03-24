bb7b501379779b26781b142859b7186144d41d5b
Mokhles <mokhleshmd1@gmail.com>
2018-03-24 01:54:13 +0100

    third commit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,9 +10,85 @@ export class ProductsComponent implements OnInit {
   products: any;
   cartProducts: any;
 
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    const data = localStorage.getItem('card');
+    if (data !== null) {
+      this.cartProducts = JSON.parse(data);
+    } else {
+      this.cartProducts = [];
+    }
+
+    this.products = [
+      {
+        id: 1,
+        title: 'Samsung',
+        description: 'bla bla bla',
+        img: 'assets/1.png',
+        price: 1200
+      },
+      {
+        id: 2,
+        title: 'lenovo',
+        description: 'bla bla',
+        img: 'assets/2.png',
+        price: 800
+      },
+      {
+        id: 3,
+        title: 'hp',
+        description: 'haja behya',
+        img: 'assets/3.png',
+        price: 3
+      },
+      {
+        id: 4,
+        title: 'mac pro',
+        description: 'khorda',
+        img: 'assets/4.png',
+        price: 3
+      },
+      {
+        id: 5,
+        title: 'hp wehed ekher',
+        description: 'allahou a3lem',
+        img: 'assets/5.png',
+        price: 4
+      },
+      {
+        id: 6,
+        title: 'msi rog',
+        description: 'bla bla',
+        img: 'assets/6.png',
+        price: 3000
+      },
+      {
+        id: 7,
+        title: 'mac book pro',
+        description: 'pro',
+        img: 'assets/7.png',
+        price: 4000
+      },
+      {
+        id: 8,
+        title: 'lenovo gamer',
+        description: 'gamer m3ah manette',
+        img: 'assets/8.png',
+        price: 2000
+      },
+      {
+        id: 9,
+        title: 'pc gamer',
+        description: 'blabla',
+        img: 'assets/9.png',
+        price: 3
+      }
+    ];
+  }
+
+
+  goToCard() {
+    this.router.navigate(['/card']);
   }
 }
