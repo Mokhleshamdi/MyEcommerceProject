640994c27c2ff4be71efb5c265fb661a549ee079
Mokhles <mokhleshmd1@gmail.com>
2018-03-24 01:13:01 +0100

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {routes} from './app.router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { CardComponent } from './card/card.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    CardComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routes
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,16 +1,16 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AboutAsComponent} from './about-as/about-as.component';
+import {AboutComponent} from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
-import { CartComponent } from './cart/cart.component';
+import { CardComponent } from './card/card.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 export const router: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'about', component: AboutAsComponent},
+  {path: 'about', component: AboutComponent},
   {path: 'products', component: ProductsComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'card', component: CartComponent},
+  {path: 'card', component: CardComponent},
   {path: 'sidebar-component', component: SidebarComponent},
   ];
 export const routes: ModuleWithProviders = RouterModule.forRoot(router)
